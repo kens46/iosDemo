e15f1c583794f7c42b3d471430235f794e7b7f72
admin <admin@admins-macbook-air.local>
2018-08-20 16:17:57 +0700

    test

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -9,10 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var HelloLabel: UILabel!
+    @IBOutlet weak var txtName: UITextField!
+    
+    @IBAction func clickButton(_ sender: Any) {
+        self.HelloLabel.text = "Hello \(self.txtName.text!)"
+    }
+    
+    @IBAction func changeTheme(_ sender: Any) {
+        view.backgroundColor = UIColor.black
+        for eachView in view.subviews{
+            eachView.backgroundColor =
+                UIColor.black
+            if let lable = eachView as? UILabel{
+                lable.textColor = UIColor.white
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.HelloLabel.text = "hello world after loaded"
     }
 
     override func didReceiveMemoryWarning() {
